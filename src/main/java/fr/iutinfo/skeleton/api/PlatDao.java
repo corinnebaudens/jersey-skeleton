2cabe3b41d82fd51fe11fca7d4f760ecdfbc3824
Olivier CAPELLE <capelleo@hevea08.iut-infobio.priv.univ-lille1.fr>
2016-03-31 11:11:55 +0200

    Fin création PlatDao

--- src/main/java/fr/iutinfo/skeleton/api/PlatDao.java
+++ src/main/java/fr/iutinfo/skeleton/api/PlatDao.java
@@ -1,5 +1,37 @@
 package fr.iutinfo.skeleton.api;
 
-public interface PlatDao {
+import java.util.List;
 
+import org.skife.jdbi.v2.sqlobject.Bind;
+import org.skife.jdbi.v2.sqlobject.BindBean;
+import org.skife.jdbi.v2.sqlobject.GetGeneratedKeys;
+import org.skife.jdbi.v2.sqlobject.SqlQuery;
+import org.skife.jdbi.v2.sqlobject.SqlUpdate;
+import org.skife.jdbi.v2.sqlobject.customizers.RegisterMapperFactory;
+import org.skife.jdbi.v2.tweak.BeanMapperFactory;
+
+public interface PlatDao {
+	@SqlUpdate("create table plats (id integer primary key autoincrement, name varchar(50))")
+	void createPlatTable();
+
+	@SqlUpdate("insert into plats (name) values (:name)")
+	@GetGeneratedKeys
+	int insert(@BindBean() Plat plat);
+
+	@SqlQuery("select * from plats where name = :name")
+    @RegisterMapperFactory(BeanMapperFactory.class)
+	Plat findByName(@Bind("name") String name);
+
+	@SqlUpdate("drop table if exists plats")
+	void dropPlatTable(); 
+
+	@SqlQuery("select * from plats order by id")
+	@RegisterMapperFactory(BeanMapperFactory.class)
+	List<Plat> all();
+
+	@SqlQuery("select * from plats where id = :id")
+	@RegisterMapperFactory(BeanMapperFactory.class)
+	Plat findById(@Bind("id") int id);
+
+	void close();
 }
