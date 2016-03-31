2a80a09583c8686f04741549ebefbe5cbb143e8b
matthip <matthias.pottiez@laposte.net>
2016-03-31 11:10:55 +0200

    Création de la classe Plat

---
+++ src/main/java/fr/iutinfo/skeleton/api/Plat.java
@@ -0,0 +1,38 @@
+package fr.iutinfo.skeleton.api;
+
+public class Plat {
+	
+	private String nom;
+	private String cuisinier;
+	private int id;
+	private int quantitePart;
+	
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getCuisinier() {
+		return cuisinier;
+	}
+	public void setCuisinier(String cuisinier) {
+		this.cuisinier = cuisinier;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getQuantitePart() {
+		return quantitePart;
+	}
+	public void setQuantitePart(int quantitePart) {
+		this.quantitePart = quantitePart;
+	}
+	
+	public String toString() {
+		return nom + " fait par : " + this.cuisinier;
+	}
+}
