76964ff3c0d6fab13b1dc2093c512fe28740733a
GregoireDurand <gregoire.durand@gmail.com>
2016-04-01 11:59:59 +0200

    Classe CommandeDao en cours de rédaction

--- src/main/java/fr/iutinfo/skeleton/api/CommandeDao.java
+++ src/main/java/fr/iutinfo/skeleton/api/CommandeDao.java
@@ -11,5 +11,10 @@ import org.skife.jdbi.v2.sqlobject.customizers.RegisterMapperFactory;
 import org.skife.jdbi.v2.tweak.BeanMapperFactory;
 
 public interface CommandeDao {
-
+	@SqlUpdate("create table commandes (id integer primary key autoincrement, idclient integer(6), idplat integer(6), quantitecde integer(2))")
+	void createCommande();
+	
+	@SqlUpdate("insert into commandes (idclient,idplat,quantitecde) values (:id_client, :id_plat, :quantité")
+	void insert(@BindBean() Commande commande);
+	
 }
