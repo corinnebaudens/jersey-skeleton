4e99e8332f476507d003219e9a4d24a66ae7ef6c
dubromev <vincent.dubromez+git@gmail.com>
2016-04-01 09:59:16 +0200

    Ajout du role dans le constructeur

--- src/main/java/fr/iutinfo/skeleton/api/UserDBResource.java
+++ src/main/java/fr/iutinfo/skeleton/api/UserDBResource.java
@@ -3,7 +3,7 @@ package fr.iutinfo.skeleton.api;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import fr.iutinfo.skeleton.utils.Role;
+import fr.iutinfo.skeleton.api.Role;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
