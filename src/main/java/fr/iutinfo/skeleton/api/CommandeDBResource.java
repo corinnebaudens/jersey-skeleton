67fd74e8fac0099e520e708165d5d73ca4c1d58d
dubromev <vincent.dubromez+git@gmail.com>
2016-04-01 14:12:14 +0200

    Update constructeur CommandeDBResource

--- src/main/java/fr/iutinfo/skeleton/api/CommandeDBResource.java
+++ src/main/java/fr/iutinfo/skeleton/api/CommandeDBResource.java
@@ -1,7 +1,33 @@
 package fr.iutinfo.skeleton.api;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Path("/cmddb")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class CommandeDBResource {
 	
 	// En cours d'implémentation par merianb/dubromev
-
+	
+	private static CommandeDao dao = BDDFactory.getDbi().open(CommandeDao.class);
+	final static Logger logger = LoggerFactory.getLogger(CommandeDBResource.class);
+	
+	
+	public CommandeDBResource() {
+		try {
+			dao.createCommandeTable();
+			dao.insert(new Commande(0, 0, 0, 1));
+		} catch (Exception e) {
+			logger.debug("Table Commande déjà là !");
+		}
+	}
+	
+	
+	
 }
