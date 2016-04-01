e13f0767cd4df0c27dff627624f04cb9e100b88f
GregoireDurand <gregoire.durand@gmail.com>
2016-04-01 11:21:56 +0200

    Création classe CommandeDao.java

---
+++ src/main/java/fr/iutinfo/skeleton/api/CommandeDao.java
@@ -0,0 +1,15 @@
+package fr.iutinfo.skeleton.api;
+
+import java.util.List;
+
+import org.skife.jdbi.v2.sqlobject.Bind;
+import org.skife.jdbi.v2.sqlobject.BindBean;
+import org.skife.jdbi.v2.sqlobject.GetGeneratedKeys;
+import org.skife.jdbi.v2.sqlobject.SqlQuery;
+import org.skife.jdbi.v2.sqlobject.SqlUpdate;
+import org.skife.jdbi.v2.sqlobject.customizers.RegisterMapperFactory;
+import org.skife.jdbi.v2.tweak.BeanMapperFactory;
+
+public interface CommandeDao {
+
+}
