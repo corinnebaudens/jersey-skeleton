9628d8119c21736ec692c4511a1effe29f8e68ec
dubromev <vincent.dubromez+git@gmail.com>
2016-04-01 16:05:09 +0200

    Mise à jour CommandeDBResource

--- src/main/java/fr/iutinfo/skeleton/api/CommandeDBResource.java
+++ src/main/java/fr/iutinfo/skeleton/api/CommandeDBResource.java
@@ -1,8 +1,15 @@
 package fr.iutinfo.skeleton.api;
 
+import java.util.List;
+
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 
 import org.slf4j.Logger;
@@ -28,6 +35,45 @@ public class CommandeDBResource {
 		}
 	}
 	
+	@POST
+	public Commande createCommande(Commande commande){
+		int id_commande = dao.insert(commande);
+		commande.setId_commande(id_commande);
+		return commande;
+	}
 	
+//	@GET
+//	@Path("/{id_commande}")
+//	public Commande getCommande(@PathParam("id") int id_commande) {
+//		Commande commande = dao.findById(id_commande);
+//		if (commande == null) {
+//			throw new WebApplicationException(404);
+//		}
+//		return commande;
+//	}
+	
+	@GET
+	@Path("/{id_plat}")
+	public int getQuantiteCde(@PathParam("idplat") int id_plat) {
+		int sumQuantiteCde = dao.quantiteCommande(id_plat);
+		return sumQuantiteCde;
+	}
+	
+	@GET
+	@Path("/{id_client}")
+	public List<Commande> getListeCdeParClient(@PathParam("idclient") int id_client) {
+		List<Commande> listeCde = dao.listeCdeParClient(id_client);
+		return listeCde;
+	}
+	
+//	@GET
+//	public List<Commande> getAllCommandes(@QueryParam("q") String query) {
+//		logger.debug(query);
+//		
+//		if (query == null || query.isEmpty())
+//			return dao.all();
+//		else
+//			return dao.listeCdeParClient("%" + query + "%");
+//	}
 	
 }
