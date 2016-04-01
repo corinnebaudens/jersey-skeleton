4c471517a4a5198f023af57662be297569492e24
dubromev <vincent.dubromez+git@gmail.com>
2016-04-01 14:07:08 +0200

    Ajout @param quantitecde classe Commande

--- src/main/java/fr/iutinfo/skeleton/api/Commande.java
+++ src/main/java/fr/iutinfo/skeleton/api/Commande.java
@@ -5,14 +5,16 @@ public class Commande {
 	private int id_commande;
 	private int id_client;
 	private int id_plat;
+	private int quantitecde;
 	
 	public Commande() {
 	}
 	
-	public Commande(int id_commande, int id_client, int id_plat){
+	public Commande(int id_commande, int id_client, int id_plat, int quantitecde){
 		this.id_commande = id_commande;
 		this.id_client = id_client;
 		this.id_plat = id_plat;
+		this.quantitecde = quantitecde;
 	}
 
 	public int getId_commande() {
@@ -37,6 +39,14 @@ public class Commande {
 
 	public void setId_plat(int id_plat) {
 		this.id_plat = id_plat;
+	}
+
+	public int getQuantitecde() {
+		return quantitecde;
+	}
+
+	public void setQuantitecde(int quantitecde) {
+		this.quantitecde = quantitecde;
 	}
 
 	@Override
